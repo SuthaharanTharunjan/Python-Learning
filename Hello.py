fbf9cec55e34aee9e24c526eba6dc2094263c2da
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-06-04 23:35:48 +0530

    Fix variable naming for mass and velocity input prompts

--- Hello.py
+++ Hello.py
@@ -1,8 +1,11 @@
 mass=float(input("Enter your mass: "))
-unitofmass=input("Enter the unit of mass (kg or g): ")
-if unitofmass=="g":
+unit_of_mass=input("Enter the unit of mass (kg or g): ")
+if unit_of_mass=="g":
     mass=mass/1000
 velocity=float(input("Enter your velocity in m/s: "))
+unit_of_velocity=input("Enter the unit of velocity (m/s or km/h): ")
+if unit_of_velocity=="km/h":
+    velocity=velocity/3.6
 kinetic_energy=0.5*mass*velocity**2
 print("Your kinetic energy is:", kinetic_energy, "Joules")
 print("Thank you for using this programme")
