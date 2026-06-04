95fd1b994fd9c2c538fa5f955f9f3f22fb321774
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-06-04 23:32:45 +0530

    Enhance mass input to support unit selection for kinetic energy calculation

--- Hello.py
+++ Hello.py
@@ -1,4 +1,7 @@
-mass=float(input("Enter your mass in kg: "))
+mass=float(input("Enter your mass: "))
+unitofmass=input("Enter the unit of mass (kg or g): ")
+if unitofmass=="g":
+    mass=mass/1000
 velocity=float(input("Enter your velocity in m/s: "))
 kinetic_energy=0.5*mass*velocity**2
 print("Your kinetic energy is:", kinetic_energy, "Joules")
