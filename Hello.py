dcc2aaa37a4dfe4a6b2ccf34153794f3f6806eb8
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-06-04 23:28:10 +0530

    Add kinetic energy calculation program

---
+++ Hello.py
@@ -0,0 +1,5 @@
+mass=float(input("Enter your mass in kg: "))
+velocity=float(input("Enter your velocity in m/s: "))
+kinetic_energy=0.5*mass*velocity**2
+print("Your kinetic energy is:", kinetic_energy, "Joules")
+print("Thank you for using this programme")
