b9822174211724bd47d056d3207aff929c3853ef
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-06-08 00:11:25 +0530

    Add input validation for user-defined number ranges in hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+
+y=int(input("Enter a number from 1 to 500: "))
+while y<1 or y>500:
+    print("Invalid input. Please enter a number from 1 to 500.")
+    y=int(input("Enter a number from 1 to 500: "))
+x=int(input("Enter a number from 500 to 1000: "))
+while x<500 or x>1000:
+    print("Invalid input. Please enter a number from 100 to 1000.")
+    x=int(input("Enter a number from 500 to 1000: "))
+    
+
