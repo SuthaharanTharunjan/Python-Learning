c971e8d14966896b6f55e9cff4d4e36c061d90c1
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-06-08 22:06:35 +0530

    Refactor input handling in hello.py to ensure validation loops are correctly nested and improve code readability

---
+++ Random_Thing_I_wrote.py
@@ -0,0 +1,26 @@
+restart=1
+while restart==1:
+    y=int(input("Enter a number from 1 to 500: "))
+    while y<1 or y>500:
+        print("Invalid input. Please enter a number from 1 to 500.")
+        y=int(input("Enter a number from 1 to 500: "))
+    x=int(input("Enter a number from 500 to 1000: "))
+    while x<500 or x>1000:
+        print("Invalid input. Please enter a number from 500 to 1000.")
+        x=int(input("Enter a number from 500 to 1000: "))
+    z=x-y
+    if   0<z<166.67:
+        print("1")
+    elif 166.67<=z<333.34:
+        print("2")
+    elif 333.34<=z<500:
+        print("3")     
+    elif 500<=z<666.67:
+        print("4")
+    elif 666.67<=z<833.34:
+        print("5")
+    elif 833.34<=z<1000:
+        print("6")
+
+    restart=int(input("Do you want to restart? (1 for yes, 0 for no): "))
+    
