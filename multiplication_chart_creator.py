73478c847805532bb6357211ecccc759c1d1cb32
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-06-14 22:40:38 +0530

    Fix input type for multiplication chart creator to ensure float handling

--- multiplication_chart_creator.py
+++ multiplication_chart_creator.py
@@ -1,4 +1,4 @@
-x=int(input("Enter a number to create its multiplication chart: "))
+x=float(input("Enter a number to create its multiplication chart: "))
 i=int(input("Enter the number of multiples to display: "))
 for i in range(1, i+1):
     print(i,"X",x,"=", i*x)
