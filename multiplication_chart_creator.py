7e0d90cba8bc12c2d491849dec7bc2fe3ca980f5
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-06-14 22:37:09 +0530

    Update multiplication chart creator to allow user-defined number of multiples

--- multiplication_chart_creator.py
+++ multiplication_chart_creator.py
@@ -1,3 +1,4 @@
 x=int(input("Enter a number to create its multiplication chart: "))
-for i in range(1, 11):
+i=int(input("Enter the number of multiples to display: "))
+for i in range(1, i+1):
     print(i,"X",x,"=", i*x)
