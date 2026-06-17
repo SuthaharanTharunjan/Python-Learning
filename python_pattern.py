9d9f9274ca14bddbd8e1543a5274209ca7d0aebf
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-06-17 20:51:06 +0530

    add new shape

--- python_pattern.py
+++ python_pattern.py
@@ -1,6 +1,19 @@
+'''
 n=int(input("Enter a number: "))
 while n>0:
     for i in range(n):
         print("*",end="")
     n=n-1
-    print()    
+    print()
+'''
+
+n=int(input("Enter a number: "))
+k=1
+while n>0:
+    for i in range(n):
+        print("*",end="") 
+    print()
+    print(" "*k,end="")
+    k=k+1
+    n=n-2     
+
