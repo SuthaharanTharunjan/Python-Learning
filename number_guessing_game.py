285e9da95c34cea3cac45f757823f02e221b6b7b
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-06-17 19:50:56 +0530

    Refactor number guessing game to allow replaying after each round

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,11 +1,15 @@
 import random
-num=random.randint(1,100)
-guess=int
-while guess!=num:
-    guess=int(input("Enter a number between 1 and 100: "))
-    if guess<num:
-        print("Its higher than that")
-    elif guess>num:
-        print("Its lower than that")
-    else:
-        print("You guessed it right!")        
+while True:
+    num=random.randint(1,100)
+    guess=int
+    while guess!=num:
+        guess=int(input("Enter a number between 1 and 100: "))
+        if guess<num:
+            print("Its higher than that")
+        elif guess>num:
+            print("Its lower than that")
+        else:
+            print("You guessed it right!")
+    if input("Do you want to play again? (y/n): ").lower() != 'y':
+        break
+print("Thanks for playing!")
