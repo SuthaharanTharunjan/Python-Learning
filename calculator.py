bbfeb40fbfdf7d5ae5845649df1e55348bf2a6f0
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-06-21 21:52:13 +0530

    Add basic calculator functionality for addition, subtraction, multiplication, and division

---
+++ calculator.py
@@ -0,0 +1,14 @@
+while True:
+    num_1=float(input("Enter number 1: "))
+    num_2=float(input("Enter number 2: "))
+    function=str(input("Enter the function:"))
+    if function=="*":
+        print(num_1*num_2)
+    elif function=="/":
+        print(num_1/num_2)
+    elif function=="+":
+        print(num_1+num_2)
+    elif function=="-":
+        print(num_1-num_2)
+
+
