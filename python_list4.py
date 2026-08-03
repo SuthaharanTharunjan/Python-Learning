afc26490313f7aa227fbe93fcd126a2f892fdd06
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-08-03 22:26:50 +0530

    Refactor digit summation logic to iterate while max number is greater than or equal to 10

--- python_list4.py
+++ python_list4.py
@@ -1,7 +1,12 @@
 numbers=str(input("Enter numbers with space as separator: "))
 list=[int(x) for x in numbers.split()]
 list2=[]
-for i in list:
-    digit=[int(x) for x in str(i)]
-    list2.append(sum(digit))
-print(list2)
+while max(list) >= 10:
+    for i in list:
+        digit=[int(x) for x in str(i)]
+        list2.append(sum(digit))
+    print(list2)
+    list.clear()
+    list=list2.copy()
+    list2.clear()
+print(f"Final list is: {list}")
