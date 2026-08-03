0ef52d5f5d2a06157ec93666f6eda7424603cba3
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-08-03 21:18:42 +0530

    Add print statements for largest, smallest, and sum of numbers in the list

--- python_list3.py
+++ python_list3.py
@@ -3,10 +3,13 @@ list=[int(x) for x in numbers.split()]
 list.sort()
 list2=[]
 [list2.append(x) for x in list if x not in list2]
+print(f"largest number is: {list2[-1]}")
 print(f"Second largest number is: {list2[-2]}")
+print(f"Smallest number is: {list2[0]}")
 list3=[]
 [list3.append(x) for x in list2 if x%2==0]
 print(f"Even numbers are: {list3}")
 list4=[]
 [list4.append(x) for x in list2 if x%2!=0]
 print(f"Odd numbers are: {list4}")
+print(f"Sum of all numbers is: {sum(list2)}")
