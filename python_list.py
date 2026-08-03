761a8b5046a2041653221e3eb98b5a6416a3b4e0
SuthaharanTharunjan <S.Tharunjan2023@outlook.com>
2026-08-03 20:11:38 +0530

    Add script to find largest and smallest numbers from user input

---
+++ python_list.py
@@ -0,0 +1,9 @@
+number=str(input("Enter number with space as seperator: "))
+list1=number.split(" ")
+list2=[int(x) for x in list1]
+list2.sort()
+print(list2)
+largest=max(list2)
+smallest=min(list2)
+print("Largest:", largest)
+print("Smallest:", smallest)
